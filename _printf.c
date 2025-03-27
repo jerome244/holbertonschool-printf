277708975794880f633961bfe07b434d079fedd6
root <10485@holbertonstudents.com>
2025-03-27 15:14:58 +0000

    fix: adding commentary line 25 for the usage of an if statement for checking if the inputted string contains a % character

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') //check if the inputted string contained the '%' character
 		{
 			if (format[i + 1] != '\0')
 			{
